24a8490ffe78559987ccd0ab0b6abca69b6caf7e
kakato <kakato1030@gmail.com>
2026-02-07 10:06:07 +0900

    chore: bump mm version

--- mm.rb
+++ mm.rb
@@ -4,16 +4,16 @@
 class Mm < Formula
   desc "Personal knowledge management CLI tool (PKOS - GTD, Bullet Journal, Zettelkasten)"
   homepage "https://github.com/kkato1030/mm"
-  version "0.1.3"
+  version "0.1.4"
   license "AGPL-3.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/kkato1030/mm/releases/download/v0.1.3/mm-0.1.3-darwin-arm64"
+      url "https://github.com/kkato1030/mm/releases/download/v0.1.4/mm-0.1.4-darwin-arm64"
       sha256 "2348f0c94d0a7fbb6c0f384be4201a70153254a3e7e18320f6853282adeefd4d"
 
       def install
-        bin.install "mm-0.1.3-darwin-arm64" => "mm"
+        bin.install "mm-0.1.4-darwin-arm64" => "mm"
       end
     end
   end
